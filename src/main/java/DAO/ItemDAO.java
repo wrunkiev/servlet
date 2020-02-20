c52021a215b1d7800ca147e3bff64f3179a0e0e0
PotapenkoS <wrunkiev@ukr.net>
2020-02-20 18:06:34 +0200

    servlet2

--- src/main/java/DAO/ItemDAO.java
+++ src/main/java/DAO/ItemDAO.java
@@ -15,10 +15,8 @@ public class ItemDAO {
         checkItemNull(item);
         existItem(item);
 
-        Session session = null;
         Transaction tr = null;
-        try{
-            session = createSessionFactory().openSession();
+        try (Session session = createSessionFactory().openSession()) {
             tr = session.getTransaction();
             tr.begin();
 
@@ -26,25 +24,19 @@ public class ItemDAO {
 
             tr.commit();
             return item;
-        }catch (HibernateException e){
+        } catch (HibernateException e) {
             System.err.println("Exception in method ItemDAO.save. Save item with ID: " + item.getId() + " is failed");
             System.err.println(e.getMessage());
-            if(tr != null) {
+            if (tr != null) {
                 tr.rollback();
             }
             return null;
-        }finally {
-            if(session != null){
-                session.close();
-            }
         }
     }
 
     public Item findById(long id){
-        Session session = null;
         Transaction tr = null;
-        try{
-            session = createSessionFactory().openSession();
+        try (Session session = createSessionFactory().openSession()) {
             tr = session.getTransaction();
             tr.begin();
 
@@ -52,17 +44,13 @@ public class ItemDAO {
 
             tr.commit();
             return item;
-        }catch (HibernateException e){
+        } catch (HibernateException e) {
             System.err.println("Exception in method ItemDAO.findById. Item with ID: " + id + " is not defined in DB.");
             System.err.println(e.getMessage());
-            if(tr != null) {
+            if (tr != null) {
                 tr.rollback();
             }
             return null;
-        }finally {
-            if(session != null){
-                session.close();
-            }
         }
     }
 
@@ -73,10 +61,8 @@ public class ItemDAO {
             throw new Exception("Exception in method ItemDAO.update. Enter id for item.");
         }
 
-        Session session = null;
         Transaction tr = null;
-        try{
-            session = createSessionFactory().openSession();
+        try (Session session = createSessionFactory().openSession()) {
             tr = session.getTransaction();
             tr.begin();
 
@@ -84,17 +70,13 @@ public class ItemDAO {
 
             tr.commit();
             return item;
-        }catch (HibernateException e){
+        } catch (HibernateException e) {
             System.err.println("Exception in method ItemDAO.update. Update item with ID: " + item.getId() + " is failed");
             System.err.println(e.getMessage());
-            if(tr != null) {
+            if (tr != null) {
                 tr.rollback();
             }
             return null;
-        }finally {
-            if(session != null){
-                session.close();
-            }
         }
     }
 
@@ -105,25 +87,19 @@ public class ItemDAO {
             throw new Exception("Exception in method ItemDAO.delete. Item with ID: " + id + " is not defined in DB.");
         }
 
-        Session session = null;
         Transaction tr = null;
-        try{
-            session = createSessionFactory().openSession();
+        try (Session session = createSessionFactory().openSession()) {
             tr = session.getTransaction();
             tr.begin();
 
             session.delete(item);
 
             tr.commit();
-        }catch (HibernateException e){
+        } catch (HibernateException e) {
             System.err.println("Exception in method ItemDAO.delete. Delete item with ID: " + item.getId() + " is failed.");
             System.err.println(e.getMessage());
-            if(tr != null) {
+            if (tr != null) {
                 tr.rollback();
-            }
-        }finally {
-            if(session != null){
-                session.close();
             }
         }
     }
@@ -142,11 +118,9 @@ public class ItemDAO {
     }
 
     private static Item getItemFromDB(Item item){
-        Session session = null;
         Transaction tr = null;
         Item it;
-        try{
-            session = createSessionFactory().openSession();
+        try (Session session = createSessionFactory().openSession()) {
             tr = session.getTransaction();
             tr.begin();
 
@@ -154,26 +128,22 @@ public class ItemDAO {
             NativeQuery query = session.createNativeQuery(sql, Item.class);
             query.setParameter(1, item.getName());
 
-            it = (Item)query.getSingleResult();
+            it = (Item) query.getSingleResult();
 
             tr.commit();
             return it;
-        }catch (NoResultException ex){
+        } catch (NoResultException ex) {
             System.err.println(ex.getMessage());
-            if(tr != null) {
+            if (tr != null) {
                 tr.rollback();
             }
             return null;
-        }catch (HibernateException e){
+        } catch (HibernateException e) {
             System.err.println(e.getMessage());
-            if(tr != null) {
+            if (tr != null) {
                 tr.rollback();
             }
             return null;
-        }finally {
-            if(session != null){
-                session.close();
-            }
         }
     }
 }
